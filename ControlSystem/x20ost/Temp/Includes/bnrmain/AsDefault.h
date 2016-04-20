c81d38ad046d0c7420590d63c9961278ea928695
alexander <alex_mpowt@tut.by>
2016-04-20 22:27:54 +0300

    alex

--- ControlSystem/x20ost/Temp/Includes/bnrmain/AsDefault.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BNRMAIN_DEFAULT_
-#define _BNRMAIN_DEFAULT_
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <bnrmainTYP.h>
-#include <bnrmainVAR.h>
-#endif
--- sSystem/src/ISomeEventListener.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public interface ISomeEventListener {
-	void eventHappens(CHMessage sMsg);
-	public void RunProc();
-	public int getID();
-}
